240613170094bd8cff7b29925c2978142551a5c7
Janisha Patel <Janisha.Patel@softwire.com>
2023-08-27 22:59:17 +0100

    Div by 13 rules added

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,7 +2,7 @@ function isDivisible(num, denom){
     return (num % denom === 0);
 }
 
-for (var i = 1; i <= 105; i++)
+for (var i = 1; i <= 200; i++)
 {
     var str = "";
     if (isDivisible(i, 3)) {
@@ -14,8 +14,17 @@ for (var i = 1; i <= 105; i++)
     if (isDivisible(i, 7)) {
         str += "Bang";
     }
-    if (isDivisible(i, 11))
+    if (isDivisible(i, 11)) {
         str = "Bong";
+    }
+    if (isDivisible(i, 13)) {
+        bIndex = str.indexOf("B");
+        if(bIndex !== -1) {
+            str = (str.slice(0, bIndex) + "Fezz" + str.slice(bIndex));
+        } else {
+            str += "Fezz";
+        }
+    }
     if (str === "") {
         str = (i);
     }
