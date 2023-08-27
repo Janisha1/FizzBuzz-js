e01476d70f1e9f6c5166ec79a35bf0388d5db6d9
Janisha Patel <Janisha.Patel@softwire.com>
2023-08-27 22:00:19 +0100

    Added FizzBuzz if div by 3 and 5

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,5 +1,19 @@
-function PrintMessage(){
-    console.log("Hello World");
+
+for (var i = 1; i <= 100; i++)
+{
+    var str = "";
+    if (i % 3 === 0) {
+        str += "Fizz";
+    } 
+    if (i % 5 === 0) {
+        str += "Buzz";
+    } 
+    if (str === "") {
+        str = (i);
+    }
+
+    console.log(str);
 }
 
-PrintMessage();
+
+
