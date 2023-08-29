ce2d76739880ffb0f71deeaa4f5d8f31abe05597
Janisha Patel <Janisha.Patel@softwire.com>
2023-08-29 09:44:19 +0100

    User Entry Max Number

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -9,39 +9,65 @@ function isDivisible(num, denom){
     return (num % denom === 0);
 }
 
-for (var i = 1; i <= 200; i++)
-{
-    var str_Array = [];
-    if (isDivisible(i, 3)) {
-        str_Array.push("Fizz");
-    } 
-    if (isDivisible(i, 5)) {
-        str_Array.push("Buzz");
-    } 
-    if (isDivisible(i, 7)) {
-        str_Array.push("Bang");
-    }
-    if (isDivisible(i, 11)) {
-        str_Array.length = 0;
-        str_Array.push("Bong");
-    }
-    if (isDivisible(i, 13)) {
-        var bIndex = (str_Array.join("").indexOf("B"));
-        if(bIndex !== -1) {
-            str_Array.splice((bIndex/4), 0, "Fezz");
-        } else {
-            str_Array.push("Fezz")
+// function getUserInput(){
+//     var userInput;
+//     const readline = require("readline").createInterface({
+//         input: process.stdin,
+//         output: process.stdout,
+//     });
+
+//     readline.question("What is the Maximum number you would like to use? ", function (userInput){
+//         console.log(`Your number ${userInput}`);
+//         readline.close();
+//     });
+//     return userInput;
+// }
+
+//userMaxNum = getUserInput();
+var userInput;
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question("What is the Maximum number you would like to use? ", function (userInput){
+    doFizzBuzz(parseInt(userInput));
+    readline.close();
+});
+
+function doFizzBuzz(userInput){
+    for (var i = 1; i <= userInput; i++)
+    {
+        var str_Array = [];
+        if (isDivisible(i, 3)) {
+            str_Array.push("Fizz");
+        } 
+        if (isDivisible(i, 5)) {
+            str_Array.push("Buzz");
+        } 
+        if (isDivisible(i, 7)) {
+            str_Array.push("Bang");
         }
-    }
-    if (isDivisible(i, 17)) {
-        str_Array.reverse();
-    }
-    if (str_Array.length === 0) {
-        str_Array.push(i.toString());
-    }
+        if (isDivisible(i, 11)) {
+            str_Array.length = 0;
+            str_Array.push("Bong");
+        }
+        if (isDivisible(i, 13)) {
+            var bIndex = (str_Array.join("").indexOf("B"));
+            if(bIndex !== -1) {
+                str_Array.splice((bIndex/4), 0, "Fezz");
+            } else {
+                str_Array.push("Fezz")
+            }
+        }
+        if (isDivisible(i, 17)) {
+            str_Array.reverse();
+        }
+        if (str_Array.length === 0) {
+            str_Array.push(i.toString());
+        }
 
-    console.log(str_Array.join(""));
+        console.log(str_Array.join(""));
+    }
 }
-
-
 
